e0ad8c6bfaa34cb5111cf757791f5a30d3e9495f
Anders Conbere <anders@conbere.org>
2022-05-17 19:51:14 -0700

    fix up s3 paths

--- src/remote/mod.rs
+++ src/remote/mod.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 use std::io::Write;
+use std::path::Path;
 
 use url::Url;
 
@@ -16,7 +17,15 @@ pub async fn fetch_database(
     match remote.kind {
         RemoteKind::S3 => {
             let u = Url::parse(&remote.location)?;
-            fetch_database_s3(client, &u.host_str().unwrap(), writer).await?;
+            let directory = Path::new(u.path());
+            fetch_database_s3(
+                client,
+                &remote.name,
+                &u.host_str().unwrap(),
+                &directory,
+                writer,
+            )
+            .await?;
             Ok(())
         }
         RemoteKind::Local => Ok(()),
@@ -25,9 +34,12 @@ pub async fn fetch_database(
 
 pub async fn fetch_database_s3(
     client: &Client,
+    remote_name: &str,
     bucket: &str,
+    directory: &Path,
     writer: &mut dyn Write,
 ) -> Result<(), Box<dyn Error>> {
-    download_object(&client, bucket, "./sssync/sssync.db", writer).await?;
+    let object_path = directory.join(remote_name).join("./sssync/sssync.db");
+    download_object(&client, bucket, object_path.to_str().unwrap(), writer).await?;
     Ok(())
 }
